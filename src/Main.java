31e8ca525f68696ae6fa4fbd4a095cc7d34b02da
Chu Tinh <tinhteng@gmail>
2022-05-09 11:43:59 +0700

    java

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 10; // khai báo biến i kiểu int với giá trị là 10
+        float f = 20.5f; // khai báo biến f có kiểu float với giá trị là 20.5
+        double d = 20.5; // khai báo biến d có kiểu double với gtri là 20.5
+        boolean b = true; // khai báo biến b là kiểu boolean với gtri là true
+        char c = 'a'; // khai báo biến c có kiểu là char với gtri là kí tự a
+        String s = "Hà Nội"; // khai báo biến s có kiểu String có gtri là chuỗi Hà Nội
+
+        System.out.println("i = " + i);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("s = " + s);
+    }
+}
